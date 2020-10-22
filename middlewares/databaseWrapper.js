b0737445fd556604433ca687845c0fddc8c0ca2f
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2020-10-22 16:33:05 +0100

    fix typo

--- middlewares/databaseWrapper.js
+++ middlewares/databaseWrapper.js
@@ -3,17 +3,19 @@ import bookshelf from 'bookshelf'
 
 import dbConfig from '../knexfile'
 
-export default () => {
+export const wrapper = () => {
   const db = dbManager({ config: dbConfig })
   return {
     before: (handler, next) => {
-      db.before(handler)
+      db.before(handler, () => {})
       Object.assign(handler.context, { knex: handler.context.db, db: bookshelf(handler.context.db) })
       next()
     },
     after: (handler, next) => {
-      db.after(handler)
+      db.after(handler, () => {})
       next()
     }
   }
 }
+
+export default wrapper
